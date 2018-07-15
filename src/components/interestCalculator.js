240439b0813035750ce298eaa9eb8877d3061f03
shubham-verma061292 <40765259+shubham-verma061292@users.noreply.github.com>
2018-07-16 00:33:04 +0530

    Add files via upload

---
+++ src/components/interestCalculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../css/home.css'
+import './../logo_new.jpg';
+export class InterestCalculator extends Component {
+  render() {
+    return (
+      <div class="">
+          <h1>Interest Calculator</h1>
+      </div>
+    );
+  }
+}
+
+export default InterestCalculator;
