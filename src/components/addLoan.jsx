5cdba8b7f0d9a11334283e6f9c312c5a453086a9
shubham-verma061292 <40765259+shubham-verma061292@users.noreply.github.com>
2018-09-12 15:22:33 +0530

    Update addLoan.jsx

--- src/components/addLoan.jsx
+++ src/components/addLoan.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './../css/loan.css'
 class AddLoan extends Component {
     constructor(props) {
         super(props);
@@ -21,6 +22,10 @@ class AddLoan extends Component {
         this.setState({ [name]: value });
     };
 
+    componentDidMount() {
+        console.log(this.props);
+    }
+
     handleSubmit = event => {
         console.log("A name was submitted: " + this.state, this.state.typeOfJwel);
         fetch('https://jsonplaceholder.typicode.com/posts/12')
@@ -33,10 +38,21 @@ class AddLoan extends Component {
     render() {
 
         return (<React.Fragment>
-            <h2 className="text-center">Add Loan Information</h2>
+            <h2 className="text-center mT20">Add Loan Information</h2>
             <div className="container">
                 <div className="row">
                     <form className="mT10 center" onSubmit={this.handleSubmit}>
+                        <div className="">
+                            <span className="bFont col25">Type of Jwel :</span>
+                            <span className="col75">   <select name="typeOfJwel"
+                                className="selectBox" value={this.state.typeOfJwel} onChange={this.handleInputChange}>
+                                <option value="gold">Gold</option>
+                                <option value="silver">Silver</option>
+                                <option value="diamond">Diamond</option>
+                                <option value="platinum">Platinum</option>
+                            </select>
+                            </span>
+                        </div>
                         <div className="">
                             <span className="col25 "><label className="bFont">Name :</label></span>
                             <span className="col75 input"><input type="text" className="inputBox" name="name" value={this.state.name} onChange={this.handleInputChange} />
@@ -47,17 +63,7 @@ class AddLoan extends Component {
                             <span className="col75"><input className="inputBox" type="text" name="village" value={this.state.village} onChange={this.handleInputChange} />
                             </span>
                         </div>
-                        <div className="">
-                            <span className="col25"><label className="bFont">Type of Jwel :</label></span>
-                            <span className="col75">   <select name="typeOfJwel"
-                                className="selectBox" value={this.state.typeOfJwel} onChange={this.handleInputChange}>
-                                <option value="gold">Gold</option>
-                                <option value="silver">Silver</option>
-                                <option value="diamond">Diamond</option>
-                                <option value="platinum">Platinum</option>
-                            </select>
-                            </span>
-                        </div>
+
                         <div className="">
                             <span className="col25"><label className="bFont">Weight :</label></span>
                             <span className="col75"><input type="text" className="inputBox"
@@ -88,6 +94,7 @@ class AddLoan extends Component {
                             <span className="col75"><input type="Date" name="date" className="inputBox" placeholder="Date" value={this.state.date} onChange={this.handleInputChange} />
                             </span>
                         </div>
+
                         <button type="submit" className="btn btn-info btn-sm">Submit</button>
                     </form>
                 </div>
